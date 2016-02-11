5be8734875f94fdf6ca380c9399e445858f94a0e
Hentie Louw <hentie.louw@gmail.com>
2016-02-11 08:37:35 +0000

    Remove deprecated tests

---
+++ src/test/scala/hlouw/akka/http/sandbox/RabbitResourceSpec.scala
@@ -0,0 +1,15 @@
+package hlouw.akka.http.sandbox
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest._
+
+class RabbitResourceSpec extends FlatSpec with Matchers with ScalatestRouteTest {
+
+  "Service" should "respond to a request for events" in {
+    Pending
+  }
+
+  it should "post an event" in {
+    Pending
+  }
+}
